249a7763b94e08148d551ef0b302354eab1e3a38
SuperManfred <Manfred.Neustifter@gmail.com>
2021-02-10 20:46:25 +1000

    06 form handlerSubmit(event) function for jsx onSubmit

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -13,16 +13,21 @@ function UsernameForm({onSubmitUsername}) {
   // you prefer from the options mentioned in the instructions)
   // 💰 For example: event.target.elements[0].value
   // 🐨 Call `onSubmitUsername` with the value of the input
-
+  function handleSubmit(event) {
+    event.preventDefault()
+    // alert(event.target.elements.usernameInput.value)
+    const value = event.target.elements.usernameInput.value
+    onSubmitUsername(value)
+    console.dir(event.target.elements.usernameInput.value)
+  } 
   // 🐨 add the onSubmit handler to the <form> below
-
   // 🐨 make sure to associate the label to the input.
   // to do so, set the value of 'htmlFor' prop of the label to the id of input
   return (
-    <form>
+    <form onSubmit={handleSubmit} >
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label htmlFor="usernameInput" >Username:</label>
+        <input type="text" id="usernameInput" />
       </div>
       <button type="submit">Submit</button>
     </form>
